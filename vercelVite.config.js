da5364e0f5a220ad4c7cfeb5d8e7ba935108513b
Riley McMaster <rileymcmaster@gmail.com>
2025-03-24 17:06:24 -0400

    adjust config

--- vercelVite.config.js
+++ vercelVite.config.js
@@ -2,18 +2,18 @@ import react from "@vitejs/plugin-react";
 
 // const isCodeSandbox = "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
 
-export default {
-	plugins: [react()],
-	root: "./",
-	publicDir: "./public/",
-	base: "./",
-	server: {
-		host: true,
-		open: true, // Open if it's not a CodeSandbox
-	},
-	build: {
-		outDir: "./exampleDist",
-		emptyOutDir: true,
-		sourcemap: true,
-	},
-};
+// export default {
+// 	plugins: [react()],
+// 	root: "./",
+// 	publicDir: "./public/",
+// 	base: "./",
+// 	server: {
+// 		host: true,
+// 		open: true, // Open if it's not a CodeSandbox
+// 	},
+// 	build: {
+// 		outDir: "./exampleDist",
+// 		emptyOutDir: true,
+// 		sourcemap: true,
+// 	},
+// };
--- vite.config.js
+++ vite.config.js
@@ -2,42 +2,42 @@ import { defineConfig } from "vite";
 import * as path from "node:path";
 import react from "@vitejs/plugin-react";
 
-// export default defineConfig({
+export default defineConfig({
+	plugins: [react()],
+	root: "src/",
+	publicDir: "../public/",
+	base: "./",
+});
+
+// const dev = defineConfig({
 // 	plugins: [react()],
-// 	root: "src/",
-// 	publicDir: "../public/",
+// 	root: "./",
+// 	publicDir: "/public/",
 // 	base: "./",
+// 	server: {
+// 		host: true,
+// 		open: true, // Open if it's not a CodeSandbox
+// 	},
 // });
 
-const dev = defineConfig({
-	plugins: [react()],
-	root: "./",
-	publicDir: "/public/",
-	base: "./",
-	server: {
-		host: true,
-		open: true, // Open if it's not a CodeSandbox
-	},
-});
+// const build = defineConfig({
+// 	publicDir: true,
+// 	build: {
+// 		minify: true,
+// 		sourcemap: true,
+// 		target: "es2018",
+// 		lib: {
+// 			formats: ["cjs", "es"],
+// 			entry: "src/index.jsx",
+// 			fileName: "[name]",
+// 		},
+// 		rollupOptions: {
+// 			external: (id) => !id.startsWith(".") && !path.isAbsolute(id),
+// 			output: {
+// 				sourcemapExcludeSources: true,
+// 			},
+// 		},
+// 	},
+// });
 
-const build = defineConfig({
-	publicDir: true,
-	build: {
-		minify: true,
-		sourcemap: true,
-		target: "es2018",
-		lib: {
-			formats: ["cjs", "es"],
-			entry: "src/index.jsx",
-			fileName: "[name]",
-		},
-		rollupOptions: {
-			external: (id) => !id.startsWith(".") && !path.isAbsolute(id),
-			output: {
-				sourcemapExcludeSources: true,
-			},
-		},
-	},
-});
-
-export default process.argv[2] ? build : dev;
+// export default process.argv[2] ? build : dev;
