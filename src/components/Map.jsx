d154adde8078949180ab57be75a081d1ed8ff869
Riley McMaster <rileymcmaster@gmail.com>
2025-03-31 12:57:39 -0400

    add mirror

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -8,9 +8,10 @@ import Floor from "./Floor";
 
 const roomURL = "/assets/models/basement_3_room_1-transformed.glb";
 const ceilingURL = "/assets/models/basement_3_ceiling_1-transformed.glb";
+const mirrorURL = "/assets/models/basement_3_mirror_1-transformed.glb";
 
 const Map = (props, ref) => {
-	const [{ nodes: roomNodes }, { nodes: ceilingNodes, materials: ceilingMaterials }] = useGLTF([roomURL, ceilingURL]);
+	const [{ nodes: roomNodes }, { nodes: ceilingNodes, materials: ceilingMaterials }, { nodes: mirrorNodes, materials: mirrorMaterials }] = useGLTF([roomURL, ceilingURL, mirrorURL]);
 	const basement = roomNodes["basement_room001"];
 	const fogColor = useMemo(() => new THREE.Color("#101010"));
 
@@ -30,8 +31,11 @@ const Map = (props, ref) => {
 		<Suspense fallback={null}>
 			<group>
 				<group ref={ref} scale={3} position={[0, -0.9, 8]} dispose={null}>
-					<group {...props} dispose={null}>
+					<group>
 						<mesh geometry={ceilingNodes.basement_ceiling.geometry} material={ceilingMaterials["Material_0.013"]} scale={0.96} />
+					</group>
+					<group>
+						<mesh geometry={mirrorNodes.mirror_room002.geometry} material={mirrorMaterials["Material_0.003"]} position={[0.746, -0.095, -2.166]} />
 					</group>
 					<mesh position={[0, 1, -5]}>
 						<planeGeometry args={[4.5, 2, 1]} />
@@ -48,6 +52,6 @@ const Map = (props, ref) => {
 		</Suspense>
 	);
 };
-useGLTF.preload([roomURL, ceilingURL]);
+useGLTF.preload([roomURL, ceilingURL, mirrorURL]);
 
 export default forwardRef(Map);
