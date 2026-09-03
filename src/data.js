2caf595197495ae013b775d1151193b715fdf23e
Riley McMaster <rileymcmaster@gmail.com>
2026-09-03 10:37:46 -0400

    yt link update

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 export const links = [
 	{ title: "Instagram", slug: "instagram", href: "https://www.instagram.com/chilling_spree/" },
 	{ title: "Bandcamp", slug: "bandcamp", href: "https://billet.bandcamp.com/" },
-	{ title: "Youtube", slug: "youtube", href: "https://www.youtube.com/watch?v=m_gxWheaDyE" },
+	{ title: "Subvert", slug: "subvert", href: "https://www.subvert.fm/billet" },
+	{ title: "Youtube", slug: "youtube", href: "https://www.youtube.com/@billetgohome" },
 	// { title: "Youtube", href: "https://music.youtube.com/playlist?list=OLAK5uy_lKr5Rma8b3xPEiD3RzE3QckQinZseqoJ4" },
 	// { title: "Tidal", slug: "tidal", href: "https://tidal.com/album/399912078" },
-	{ title: "Subvert", slug: "subvert", href: "https://www.subvert.fm/billet" },
 	// { title: "Apple", slug: "apple", href: "https://music.apple.com/us/album/debris/1780205192" },
 	// { title: "Spotify", slug: "spotify", href: "https://open.spotify.com/album/6u8NFwHpBcGBw4j3HA10Hs?si=J9atBnawTv2j9ZlDzu6LwQ" },
 ];
